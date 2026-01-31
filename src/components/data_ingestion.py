a471412ae603127ff6787c8552fc16720f8ac1ea
Rohit kumawat <rohitkumawat2746@gmail.com>
2026-01-31 11:28:38 +0530

    write the code for model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.utils import save_object
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
 
 
 @dataclass
@@ -53,4 +55,8 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data) 
+    train_arr, test_arr, preprocessor_path = data_transformation.initiate_data_transformation(train_data, test_data) 
+
+
+    modelTrainer = ModelTrainer()
+    print(modelTrainer.initiate_model_trainer(train_arr,test_arr,preprocessor_path)) 
