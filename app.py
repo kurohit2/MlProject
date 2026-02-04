ca11a129bf15ff0e5cf984284720d089df584592
Rohit kumawat <rohitkumawat2746@gmail.com>
2026-02-04 20:52:22 +0530

    update port

--- app.py
+++ app.py
@@ -41,8 +41,7 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    print("App is running on http://127.0.0.1:5000")
-    app.run(host='127.0.0.1', debug=True)
+    app.run(host="0.0.0.0", port=8080)
     
             
        
