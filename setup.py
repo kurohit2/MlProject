a74d3209e9d8c9f44945321b2d04c9fefe88728a
Rohit kumawat <rohitkumawat2746@gmail.com>
2026-02-04 20:35:57 +0530

    update

--- setup.py
+++ setup.py
@@ -7,11 +7,16 @@ HYPEN_E_DOT = "-e ."
 
 def get_requirements(file_path : str) -> list[str]:
     requirements = []
-    with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.strip() for req in requirements]
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+    try:
+        with open(file_path, encoding='utf-8') as file_obj:
+            requirements = file_obj.readlines()
+    except UnicodeDecodeError:
+        with open(file_path, encoding='utf-16') as file_obj:
+            requirements = file_obj.readlines()
+
+    requirements = [req.strip() for req in requirements]
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
     return requirements
    
 
